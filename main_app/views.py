ada1d977140cf1378a560c3c2f5a9d008ab99f8d
Ryan Callebs <ryan.callebs@gmail.com>
2024-04-11 15:27:22 -0400

    part 1 completed

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+dogs = [
+  {'name': 'Layla', 'breed': 'Pitbull', 'description': 'Ghostface', 'age': 10},
+  {'name': 'Bram', 'breed': 'Swedish Mountain Dog', 'description': 'Dumb and friendly', 'age': 3},
+]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html', {
+        'dogs': dogs
+    })
+
+def dogs_index(request):
+    return render(request, 'dogs/index.html', {
+        'dogs': dogs
+    })
