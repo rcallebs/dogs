17f292bb44ebc4bf009328b911bb1d95dc5f4d31
Ryan Callebs <ryan.callebs@gmail.com>
2024-04-13 17:08:05 -0400

    part 2 complete

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+# import models
+from .models import Dog
 # Register your models here.
+admin.site.register(Dog)
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 
-dogs = [
-  {'name': 'Layla', 'breed': 'Pitbull', 'description': 'Ghostface', 'age': 10},
-  {'name': 'Bram', 'breed': 'Swedish Mountain Dog', 'description': 'Dumb and friendly', 'age': 3},
-]
+from .models import Dog
 
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
+    dogs = Dog.objects.all()
     return render(request, 'about.html', {
         'dogs': dogs
     })
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {
         'dogs': dogs
     })
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', { 'dog': dog })
